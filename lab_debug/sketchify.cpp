6f28722a9ec16a4e52fe6a662300cd1a5f2845b4
Esme Chang <esmechang17@gmail.com>
2018-09-07 14:37:22 -0500

    file submission

--- lab_debug/sketchify.cpp
+++ lab_debug/sketchify.cpp
@@ -13,29 +13,29 @@ PNG* setupOutput(unsigned w, unsigned h) {
 
 // Returns my favorite color
 HSLAPixel* myFavoriteColor(double hue) {
-  HSLAPixel p(hue, 0.8, 0.5);
-  return &p;
+  HSLAPixel * p = new HSLAPixel(hue, 0.8, 0.5);
+  return p;
 }
 
 void sketchify(std::string inputFile, std::string outputFile) {
     // Load in.png
-    PNG* original = NULL;
-
+    PNG* original = new PNG();
+    // cout << "Reached line 23" << endl;
     original->readFromFile(inputFile);
     unsigned width = original->width();
     unsigned height = original->height();
-
+    // cout << "Reached line 27" << endl;
     // Create out.png
-    PNG* output;
-    setupOutput(width, height);
+    PNG* output =  setupOutput(width, height);
+    // setupOutput(width, height);
 
     // Load our favorite color to color the outline
-    HSLAPixel* myPixel = myFavoriteColor(-1);
-
+    HSLAPixel* myPixel = myFavoriteColor(185);
+    // cout << "Reached line 36" << endl;
     // Go over the whole image, and if a pixel differs from that to its upper
     // left, color it my favorite color in the output
-    for (unsigned y = 1; 0 < y < height; y++) {
-        for (unsigned x = 1; 0 < x < width; x++) {
+    for (unsigned y = 1; 0 < y && y < height; y++) {
+        for (unsigned x = 1; 0 < x && x < width; x++) {
             // Calculate the pixel difference
             HSLAPixel& prev = original->getPixel(x - 1, y - 1);
             HSLAPixel& curr = original->getPixel(x, y);
@@ -43,13 +43,13 @@ void sketchify(std::string inputFile, std::string outputFile) {
 
             // If the pixel is an edge pixel,
             // color the output pixel with my favorite color
-            HSLAPixel currOutPixel = (*output).getPixel(x, y);
-            if (diff > 20) {
+            HSLAPixel& currOutPixel = (*output).getPixel(x, y);
+            if (diff > 20.0) {
                 currOutPixel = *myPixel;
             }
         }
     }
-
+    // cout << "Reached line 54" << endl;
     // Save the output file
     output->writeToFile(outputFile);
 
