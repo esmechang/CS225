4e8a398bdf5975d7fee5d7ce336e6a288cea041a
Esme Chang <esmesc2@linux-16.ews.illinois.edu>
2018-09-08 20:43:04 -0500

    final submission

--- mp1/mp1.cpp
+++ mp1/mp1.cpp
@@ -9,12 +9,53 @@ using cs225::HSLAPixel;
 
 
 void rotate(std::string inputFile, std::string outputFile) {
-  // TODO: Part 2
+  PNG* photo = new PNG();
+  photo->readFromFile(inputFile);
+  unsigned int w = photo->width();
+  unsigned int h = photo->height();
+  PNG* output = new PNG(w, h);
+
+  for (unsigned int x = 0; x < w; x++) {
+    for (unsigned int y = 0; y < h; y++) {
+      HSLAPixel &a = photo->getPixel(w-x-1, h-y-1);
+      HSLAPixel &b = output->getPixel(x, y);
+      b = a;
+    }
+  }
+  output->writeToFile(outputFile);
 }
 
 PNG myArt(unsigned int width, unsigned int height) {
-  PNG png(width, height);
-  // TODO: Part 3
+  PNG png = PNG(width, height);
+
+  for (unsigned int x = 0; x < width; x++) {
+    for (unsigned int y = 0; y < height; y++) {
+      HSLAPixel *gradient = new HSLAPixel(196 + x/7, 1, 0.5);
+      HSLAPixel &output = png.getPixel(x, y);
+      output = *gradient;
+    }
+  }
+
+  // for (unsigned int x = 0; x < width; x++) {
+  //   for (unsigned int y = 0; y < height; y++) {
+  //     HSLAPixel &output = png.getPixel(x, y);
+  //     output = HSLAPixel(196 + (sqrt(x*x + y*y))/(width/50), 1, 0.5);
+  //   }
+  // }
+  //
+  // for (unsigned int x = 0; x < width; x++) {
+  //   for (unsigned int y = 0; y < height; y++) {
+  //     HSLAPixel &output = png.getPixel(x, y);
+  //     output = HSLAPixel(196 + x/(width/40), 1, 0.5);
+  //   }
+  // }
+
+  // for (unsigned int x = 0; x < width; x++) {
+  //   for (unsigned int y = 0; y < height; y++) {
+  //     HSLAPixel &output = png.getPixel(x, y);
+  //     output = *color2;
+  //   }
+  // }
 
   return png;
 }
