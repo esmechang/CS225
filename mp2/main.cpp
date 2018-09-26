eb3e06958f0db7f920826a37d2cdaee1fa7012e5
Esme Chang <esmesc2@linux-09.ews.illinois.edu>
2018-09-25 23:56:10 -0500

    final submission

--- mp2/main.cpp
+++ mp2/main.cpp
@@ -24,7 +24,7 @@ int main() {
   //   Before exiting main, save your creation to disk as myImage.png
   //
   Image output;
-  output= sheet.render();
+  output = sheet.render();
   output.writeToFile("myImage.png");
 
   return 0;
