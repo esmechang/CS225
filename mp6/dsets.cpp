c58cd51386afb5668e55a9be5f43ee323baf87bf
Esme Chang <esmesc2@linux-04.ews.illinois.edu>
2018-11-12 22:42:25 -0600

    part 1

--- mp6/dsets.cpp
+++ mp6/dsets.cpp
@@ -1,1 +1,36 @@
 /* Your code here! */
+#include "dsets.h"
+
+void DisjointSets::addelements(int num) {
+  for (int i = 0; i < num; i++) {
+    v.push_back(-1);
+  }
+}
+
+int DisjointSets::find(int elem) {
+  if (v[elem] < 0) {
+    return elem;
+  } else {
+    return find(v[elem]);
+  }
+}
+
+void DisjointSets::setunion(int a, int b) {
+  int f1 = find(a);
+  int f2 = find(b);
+  int s1 = size(f1);
+  int s2 = size(f2);
+  int newSize = s1 + s2;
+  if (s1 < s2) {
+    v[f1] = f2;
+    v[f2] = newSize*-1;
+  } else {
+    v[f2] = f1;
+    v[f1] = newSize*-1;
+  }
+}
+
+int DisjointSets::size(int elem) {
+  int retval = find(elem);
+  return v[retval]*-1;
+}
--- mp6/dsets.h
+++ mp6/dsets.h
@@ -1,1 +1,16 @@
 /* Your code here! */
+#pragma once
+
+#include <cmath>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class DisjointSets {
+public:
+  void addelements(int num);
+  int find(int elem);
+  void setunion(int a, int b);
+  int size(int elem);
+  vector<int> v;
+};
